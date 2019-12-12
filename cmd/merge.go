4a5046d6554fe16e4e93074a393c8278f194145e
Alexandru Gheorghe <alexandru@sphere.me>
2019-12-12 15:28:59 +0000

    Removed redundant spaces

--- cmd/merge.go
+++ cmd/merge.go
@@ -253,7 +253,7 @@ func (c *client) notifySlackChannel(channel string, a *mergeArgs, mr *gitlab.Mer
 		Title: fmt.Sprintf("Click for MR"),
 		TitleLink: fmt.Sprintf("%s", mr.WebURL),
 		Pretext: fmt.Sprintf("🚀 merging `%s` to `%s` in *%s*", a.sourceRef, a.targetRef, a.project),
-		Text:    "",
+		Text: "",
 	}
 
 	commits, err := c.getMergeRequestCommits(mr)
